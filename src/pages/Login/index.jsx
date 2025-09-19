51b440b498f08f9797d7ff6ab3943483cd43772e
mehhul <mehhul.garg@geminisolutions.com>
2025-09-19 19:03:35 +0530

    basic layout

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Login = () => {
+    const [emailId, setEmailId] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = () =>{
+        console.log('inside handle login');
+    }
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input type="text" className="input"  value={emailId} onChange={(e) => setEmailId(e.target.value)}/>
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input type="password" className="input" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
